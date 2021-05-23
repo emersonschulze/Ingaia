bd919552a8e438bc680748c0afb89c7caf05d2c0
Emerson Schulze <emerson7e@gmail.com>
2021-05-23 16:39:26 -0300

    refactor de pasta

---
+++ Ingaia/api_1/TesteIngaia/Repository/Interface/IValorUnidadeMedidaRepository.cs
@@ -0,0 +1,28 @@
+﻿using TesteIngaia.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteIngaia.Repository.Interface
+{
+    public interface IValorUnidadeMedidaRepository
+    {
+        // api/[GET]
+        Task<IEnumerable<ValorUnidadeMedida>> GetAllTodos();
+
+        // api/1/[GET]
+        Task<ValorUnidadeMedida> GetTodo(long id);
+
+        // api/[POST]
+        Task Create(ValorUnidadeMedida todo);
+
+        // api/[PUT]
+        Task<bool> Update(ValorUnidadeMedida todo);
+
+        // api/1/[DELETE]
+        Task<bool> Delete(long id);
+
+        Task<long> GetNextId();
+    }
+}
